6ac2c3492af0f309b6a4ac5255dad282174d7f6e
pc <pc@pc.com>
2026-05-14 18:23:58 +0800

    fix cmakefile

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-
+#include <iostream>
+#include <TwoSum.hpp>
 
 int main(){
-
+	Solution obj;
+	std::vector<int> datas = {1, 2, 3, 5, 10};
+	int target = 7;
+	std::vector<int> result = obj.twoSum(datas, target);
+	for (int item : result)
+		std::cout << item << "\t";
+	std::cout << std::endl;
 	return 0;
 }
